59c8fcc0efafb31d6d100fd7715bf162a75cfc29
Gabidanety <gabinete87@gmail.com>
2025-06-01 21:06:14 -0300

    implentação de sujestoes na barra de pesquisa

---
+++ Js/script.js
@@ -0,0 +1,67 @@
+ const carrosseis = document.querySelectorAll('.curso .carrossel');
+        carrosseis.forEach(carrossel => {
+            let scrollPosition = 0;
+            const itemWidth = 200;
+            const totalItems = carrossel.children.length;
+
+            function deslizar() {
+                scrollPosition -= 1;
+                if (Math.abs(scrollPosition) >= itemWidth * totalItems) {
+                    scrollPosition = 0;
+                }
+                carrossel.style.transform = `translateX(${scrollPosition}px)`;
+            }
+
+            setInterval(deslizar, 40);
+        });
+
+        //barras de pesquisas
+
+        const barraDeBusca = document.getElementById('barraDeBusca');
+        const sugestoesContainer = document.getElementById('sugestoes');
+
+        // Lista de sugestões - depois ira vir do banco de dados
+        const sugestoes = [
+            'Programação',
+            'Banco de dados',
+            'Gestão e negócios',
+            'Línguas estrangeiras',
+            'Design e ilustração',
+            'Inteligência artificial',
+            'Saúde',
+            'Marketing'
+        ];
+
+        barraDeBusca.addEventListener('input', function () {
+            const texto = barraDeBusca.value.toLowerCase();
+            sugestoesContainer.innerHTML = ''; // Limpa sugestões anteriores
+
+            if (texto) {
+                const filtradas = sugestoes.filter(s => s.toLowerCase().includes(texto));
+
+                if (filtradas.length > 0) {
+                    sugestoesContainer.style.display = 'block'; // Mostra a div
+                    filtradas.forEach(s => {
+                        const div = document.createElement('div');
+                        div.classList.add('sugestao');
+                        div.textContent = s;
+
+                        // Clique na sugestão
+                        div.addEventListener('click', () => {
+                            barraDeBusca.value = s;
+                            sugestoesContainer.innerHTML = '';
+                            sugestoesContainer.style.display = 'none'; // Esconde de novo
+                            barraDeBusca.focus();
+                        });
+
+                        sugestoesContainer.appendChild(div);
+                    });
+                } else {
+                    sugestoesContainer.style.display = 'none'; // Nenhuma sugestão encontrada
+                }
+            } else {
+                sugestoesContainer.style.display = 'none'; // Campo vazio
+            }
+        });
+
+
